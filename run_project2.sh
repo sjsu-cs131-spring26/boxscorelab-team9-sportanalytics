a00bed032b00da44c989b6f2658c5a6dfe718fb0
minnthureinng <minn.naung@sjsu.edu>
2026-02-25 01:25:36 -0500

    Sprint2: updated PlayerStats automation with proper out dir

--- run_project2.sh
+++ run_project2.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-#Create output directory
-mkdir -p out
-
 DATA="data/PlayerStatistics.csv"
-SAMPLE="data/sample_10k.csv"
+SAMPLE="data/sample_playerstats_10k.csv"
+OUTDIR="out/playerstats"
+
+mkdir -p "$OUTDIR"
 
 echo "Creating 10k-row sample of Player Stats..."
-(head -n 1 "$DATA" && tail -n +2 "$DATA" | shuf -n 1000) > "$SAMPLE"
+(head -n 1 "$DATA" && tail -n +2 "$DATA" | shuf -n 10000) > "$SAMPLE"
 
 echo "Generating frequency table of Player teams..."
-cut -d',' -f7 "$SAMPLE" | tail -n +2 | sort | uniq -c | sort -nr > out/freq_playerteam.txt
+cut -d',' -f7 "$SAMPLE" | tail -n +2 | sort | uniq -c | sort -nr > "$OUTDIR/freq_playerteam.txt"
 
 echo "Generating top 10 highest count from frequency table..."
-head -n 10 out/freq_playerteam.txt > out/top10_playerteam.txt
+head -n 10 "$OUTDIR/freq_playerteam.txt" > "$OUTDIR/top10_playerteam.txt"
 
 echo "Generating skinny table including Id, playerteamName and points..."
-cut -d',' -f3,7,16 "$SAMPLE" | tail -n +2 | sort -u > out/skinny_table.txt
+cut -d',' -f3,7,16 "$SAMPLE" | tail -n +2 | sort -u > "$OUTDIR/skinny_playerstats.txt"
 
-echo "Project 2 run completed!!"
+echo "PlayerStatistics automation completed!!"
